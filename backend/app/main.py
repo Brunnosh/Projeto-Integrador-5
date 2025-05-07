ce41a12c707ae38ee34b1da8959b86e069a30a6a
Padreeca <g.padrecanicoletti@gmail.com>
2025-05-07 18:44:37 -0300

    update main

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,12 +3,16 @@ from app.db import SessionLocal
 from app.api import cadastro 
 from app.api import login
 from app.api import esqueceu_senha
+from app.api import receita  # Nova importação
+from app.api import despesa  # Nova importação
 
 app = FastAPI()
 
 app.include_router(cadastro.router)
 app.include_router(login.router)
 app.include_router(esqueceu_senha.router)
+app.include_router(receita.router)  # Nova rota para receitas
+app.include_router(despesa.router)  # Nova rota para despesas
 
 @app.get("/hello")
 def read_hello():
