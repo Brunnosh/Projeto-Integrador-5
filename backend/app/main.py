d43c3d3bbb89160db6ef82a45019422d8a14ea30
Bruno Savoia <brunotsavoia@gmail.com>
2025-05-12 11:30:55 -0300

    redefinição de senha babyyyy

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from app.db import SessionLocal
 from app.api import cadastro 
 from app.api import login
 from app.api import esqueceu_senha
+import os
 
 app = FastAPI()
+static_dir = os.path.join(os.path.dirname(__file__), "static")
+app.mount("/static", StaticFiles(directory=static_dir), name="static")
 
 app.include_router(cadastro.router)
 app.include_router(login.router)
