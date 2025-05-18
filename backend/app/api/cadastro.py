27982c424c3ce0d9e3e6769b4be9ecd5fe9ef593
Nicole Silvestrini <nsgarrio@gmail.com>
2025-05-18 09:38:30 -0300

    Atualizar nome e email funcionando - data de nascimento precisa de ajustes

--- backend/app/api/cadastro.py
+++ backend/app/api/cadastro.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from app.db import SessionLocal
 from app.models import login as login_model, endereco as endereco_model, dados_usuarios as usuario_model
 from app.schemas.cadastro import UsuarioCreate, EnderecoCreate
+from app.schemas.update_usuario import NomeUpdate, EmailUpdate, NascimentoUpdate
 from app.utils import security, token
 
 router = APIRouter()
@@ -102,3 +103,44 @@ def atualizar_endereco(id_endereco: int, dados: EnderecoCreate, db: Session = De
         "cep": endereco.cep,
         "id_estado": endereco.id_estado,
     }}
+
+@router.put("/atualizar-nome/{id_login}")
+def atualizar_nome(id_login: int, dados: NomeUpdate, db: Session = Depends(get_db)):
+    usuario = db.query(usuario_model.DadosUsuarios).filter_by(id_login=id_login).first()
+    if not usuario:
+        raise HTTPException(status_code=404, detail="Usuário não encontrado")
+    
+    if dados.nome:
+        usuario.nome = dados.nome
+    if dados.sobrenome:
+        usuario.sobrenome = dados.sobrenome
+
+    db.commit()
+    db.refresh(usuario)
+    return "Nome atualizado com sucesso"
+
+@router.put("/atualizar-email/{id}")
+def atualizar_email(id: int, dados: EmailUpdate, db: Session = Depends(get_db)):
+    usuario = db.query(login_model.Login).filter_by(id=id).first()
+    if not usuario:
+        raise HTTPException(status_code=404, detail="Usuário não encontrado")
+    
+    if dados.email:
+        usuario.email = dados.email
+
+    db.commit()
+    db.refresh(usuario)
+    return "Email atualizado com sucesso"
+
+@router.put("/atualizar-nascimento/{id_login}")
+def atualizar_nascimento(id_login: int, dados: NascimentoUpdate, db: Session = Depends(get_db)):
+    usuario = db.query(usuario_model.DadosUsuarios).filter_by(id_login=id_login).first()
+    if not usuario:
+        raise HTTPException(status_code=404, detail="Usuário não encontrado")
+    
+    if dados.data_nascimento:
+        usuario.data_nascimento = dados.data_nascimento
+
+    db.commit()
+    db.refresh(usuario)
+    return "Data de nascimento atualizado com sucesso"
--- frontend/lib/pages/config_page.dart
+++ frontend/lib/pages/config_page.dart
@@ -18,6 +18,16 @@ class _ConfiguracoesPageState extends State<ConfiguracoesPage> {
   String _estadosUrl = '';
   Map<int, String> estadosMap = {};
 
+  bool _editandoNome = false;
+  final _nomeController = TextEditingController();
+  final _sobrenomeController = TextEditingController();
+
+  bool _editandoEmail = false;
+  final _emailController = TextEditingController();
+
+  bool _editandoNascimento = false;
+  final _nascimentoController = TextEditingController();
+
   bool _editandoEndereco = false;
   final _cepController = TextEditingController();
   final _ruaController = TextEditingController();
@@ -63,6 +73,35 @@ class _ConfiguracoesPageState extends State<ConfiguracoesPage> {
     await _carregarEstados();
   }
 
+  Future<void> _carregarEstados() async {
+    try {
+      final response = await http.get(Uri.parse(_estadosUrl));
+      if (response.statusCode == 200) {
+        final String bodyUtf8 = utf8.decode(response.bodyBytes);
+        final List<dynamic> estados = jsonDecode(bodyUtf8);
+        setState(() {
+          estadosMap = {
+            for (var estado in estados) estado['id']: estado['nome']
+          };
+        });
+      } else {
+        print('Erro ao carregar estados: ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Erro ao carregar estados: $e');
+    }
+  }
+
+  String formatarData(String? data) {
+    if (data == null) return 'Não informado';
+    try {
+      final dataParse = DateTime.parse(data);
+      return DateFormat('dd/MM/yyyy').format(dataParse);
+    } catch (e) {
+      return 'Formato inválido';
+    }
+  }
+
   Future<void> _salvarEndereco() async {
     final idEndereco = widget.dadosUsuario['endereco']['id'];
     final isEmulator = await isRunningOnEmulator();
@@ -101,35 +140,6 @@ class _ConfiguracoesPageState extends State<ConfiguracoesPage> {
     }
   }
 
-  Future<void> _carregarEstados() async {
-    try {
-      final response = await http.get(Uri.parse(_estadosUrl));
-      if (response.statusCode == 200) {
-        final String bodyUtf8 = utf8.decode(response.bodyBytes);
-        final List<dynamic> estados = jsonDecode(bodyUtf8);
-        setState(() {
-          estadosMap = {
-            for (var estado in estados) estado['id']: estado['nome']
-          };
-        });
-      } else {
-        print('Erro ao carregar estados: ${response.statusCode}');
-      }
-    } catch (e) {
-      print('Erro ao carregar estados: $e');
-    }
-  }
-
-  String formatarData(String? data) {
-    if (data == null) return 'Não informado';
-    try {
-      final dataParse = DateTime.parse(data);
-      return DateFormat('dd/MM/yyyy').format(dataParse);
-    } catch (e) {
-      return 'Formato inválido';
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     final endereco = widget.dadosUsuario['endereco'] ?? {};
@@ -144,13 +154,151 @@ class _ConfiguracoesPageState extends State<ConfiguracoesPage> {
         padding: const EdgeInsets.all(16),
         children: [
           _buildSectionTitle('Dados Pessoais'),
-          _buildInfoCard(Icons.person, 'Nome Completo',
-              '${widget.dadosUsuario['nome']} ${widget.dadosUsuario['sobrenome']}'),
-          _buildInfoCard(Icons.email, 'E-mail', widget.dadosUsuario['email']),
-          _buildInfoCard(
-            Icons.cake,
-            'Data de Nascimento',
-            formatarData(widget.dadosUsuario['data_nascimento']),
+          // Nome
+          Card(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+            margin: const EdgeInsets.symmetric(vertical: 6),
+            elevation: 2,
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: _editandoNome
+                  ? Column(
+                      children: [
+                        _buildEditableField('Nome', _nomeController),
+                        _buildEditableField('Sobrenome', _sobrenomeController),
+                        const SizedBox(height: 8),
+                        ElevatedButton.icon(
+                          icon: const Icon(Icons.save),
+                          label: const Text("Salvar"),
+                          onPressed: _salvarNome,
+                        ),
+                      ],
+                    )
+                  : Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            const Icon(Icons.person, color: Colors.blue),
+                            const SizedBox(width: 8),
+                            Text(
+                              '${widget.dadosUsuario['nome']} ${widget.dadosUsuario['sobrenome']}',
+                              style: const TextStyle(fontSize: 16),
+                            ),
+                          ],
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.edit, color: Colors.blue),
+                          onPressed: () {
+                            setState(() {
+                              _editandoNome = true;
+                              _nomeController.text =
+                                  widget.dadosUsuario['nome'] ?? '';
+                              _sobrenomeController.text =
+                                  widget.dadosUsuario['sobrenome'] ?? '';
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+            ),
+          ),
+          // E-mail
+          Card(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+            margin: const EdgeInsets.symmetric(vertical: 6),
+            elevation: 2,
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: _editandoEmail
+                  ? Column(
+                      children: [
+                        _buildEditableField('E-mail', _emailController),
+                        const SizedBox(height: 8),
+                        ElevatedButton.icon(
+                          icon: const Icon(Icons.save),
+                          label: const Text("Salvar"),
+                          onPressed: _salvarEmail,
+                        ),
+                      ],
+                    )
+                  : Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            const Icon(Icons.email, color: Colors.blue),
+                            const SizedBox(width: 8),
+                            Text(
+                              widget.dadosUsuario['email'] ?? 'Não informado',
+                              style: const TextStyle(fontSize: 16),
+                            ),
+                          ],
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.edit, color: Colors.blue),
+                          onPressed: () {
+                            setState(() {
+                              _editandoEmail = true;
+                              _emailController.text =
+                                  widget.dadosUsuario['email'] ?? '';
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+            ),
+          ),
+          // Data de Nascimento
+          Card(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+            margin: const EdgeInsets.symmetric(vertical: 6),
+            elevation: 2,
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: _editandoNascimento
+                  ? Column(
+                      children: [
+                        _buildEditableField(
+                            'Data de Nascimento:', _nascimentoController),
+                        const SizedBox(height: 8),
+                        ElevatedButton.icon(
+                          icon: const Icon(Icons.save),
+                          label: const Text("Salvar"),
+                          onPressed: _salvarNascimento,
+                        ),
+                      ],
+                    )
+                  : Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            const Icon(Icons.cake, color: Colors.blue),
+                            const SizedBox(width: 8),
+                            Text(
+                              formatarData(
+                                  widget.dadosUsuario['data_nascimento']),
+                              style: const TextStyle(fontSize: 16),
+                            ),
+                          ],
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.edit, color: Colors.blue),
+                          onPressed: () {
+                            setState(() {
+                              _editandoNascimento = true;
+                              _nascimentoController.text = formatarData(
+                                  widget.dadosUsuario['data_nascimento']);
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+            ),
           ),
           const SizedBox(height: 24),
           _buildSectionTitle('Endereço'),
@@ -228,17 +376,128 @@ class _ConfiguracoesPageState extends State<ConfiguracoesPage> {
     );
   }
 
-  Widget _buildInfoCard(IconData icon, String label, String? value) {
-    return Card(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-      margin: const EdgeInsets.symmetric(vertical: 6),
-      elevation: 2,
-      child: ListTile(
-        leading: Icon(icon, color: Colors.blue),
-        title: Text(label),
-        subtitle: Text(value ?? 'Não informado'),
-      ),
-    );
+  Future<void> _salvarNome() async {
+    final idLogin = widget.dadosUsuario['id_login'];
+    final isEmulator = await isRunningOnEmulator();
+    final baseUrl =
+        isEmulator ? 'http://10.0.2.2:8000' : 'http://localhost:8000';
+    final url = Uri.parse('$baseUrl/atualizar-nome/$idLogin');
+
+    final body = jsonEncode({
+      "nome": _nomeController.text.trim(),
+      "sobrenome": _sobrenomeController.text.trim(),
+    });
+
+    try {
+      final response = await http.put(
+        url,
+        headers: {"Content-Type": "application/json"},
+        body: body,
+      );
+
+      if (response.statusCode == 200) {
+        setState(() {
+          widget.dadosUsuario['nome'] = _nomeController.text;
+          widget.dadosUsuario['sobrenome'] = _sobrenomeController.text;
+          _editandoNome = false;
+        });
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("Nome atualizado com sucesso")),
+        );
+      } else {
+        print('Erro: ${response.body}');
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("Erro ao atualizar nome")),
+        );
+      }
+    } catch (e) {
+      print('Erro ao enviar atualização: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Erro na comunicação com o servidor")),
+      );
+    }
+  }
+
+  Future<void> _salvarEmail() async {
+    final idLogin = widget.dadosUsuario['id_login'];
+    final isEmulator = await isRunningOnEmulator();
+    final baseUrl =
+        isEmulator ? 'http://10.0.2.2:8000' : 'http://localhost:8000';
+    final url = Uri.parse('$baseUrl/atualizar-email/$idLogin');
+
+    final body = jsonEncode({
+      "email": _emailController.text.trim(),
+    });
+
+    try {
+      final response = await http.put(
+        url,
+        headers: {"Content-Type": "application/json"},
+        body: body,
+      );
+
+      if (response.statusCode == 200) {
+        setState(() {
+          widget.dadosUsuario['email'] = _emailController.text.trim();
+          _editandoEmail = false;
+        });
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("E-mail atualizado com sucesso")),
+        );
+      } else {
+        print('Erro: ${response.body}');
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("Erro ao atualizar e-mail")),
+        );
+      }
+    } catch (e) {
+      print('Erro ao enviar atualização: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Erro na comunicação com o servidor")),
+      );
+    }
+  }
+
+  Future<void> _salvarNascimento() async {
+    final idLogin = widget.dadosUsuario['id_login'];
+    final isEmulator = await isRunningOnEmulator();
+    final baseUrl =
+        isEmulator ? 'http://10.0.2.2:8000' : 'http://localhost:8000';
+    final url = Uri.parse('$baseUrl/atualizar-nascimento/$idLogin');
+
+    final body = jsonEncode({
+      "data_nascimento": _nascimentoController.text.trim(),
+    });
+
+    try {
+      final response = await http.put(
+        url,
+        headers: {"Content-Type": "application/json"},
+        body: body,
+      );
+
+      if (response.statusCode == 200) {
+        setState(() {
+          widget.dadosUsuario['data_nascimento'] =
+              _nascimentoController.text.trim();
+          _editandoNascimento = false;
+        });
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(
+              content: Text("Data de nascimento atualizado com sucesso")),
+        );
+      } else {
+        print('Erro: ${response.body}');
+        ScaffoldMessenger.of(context).showSnackBar(
+          const SnackBar(content: Text("Erro ao atualizar data de nascimento")),
+        );
+      }
+    } catch (e) {
+      print('Erro ao enviar atualização: $e');
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Erro na comunicação com o servidor")),
+      );
+    }
   }
 
   Widget _buildEditableField(String label, TextEditingController controller) {
